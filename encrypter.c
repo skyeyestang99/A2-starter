c756efaebdd88c9dd72625470d340cb5d86803cd
skyeyestang99 <tommytang9958@gmail.com>
2022-01-26 13:16:04 -0800

    final

--- encrypter.c
+++ encrypter.c
@@ -4,14 +4,46 @@
 #include <ctype.h>
 
 #define MAX_LEN 256 // maximum length of a single word
-//char rotate(char c, int n);
-
+char rotate(char c, int n){
+    //rotate only alphate
+    if(isalpha(c)){
+      //rotate lowercase
+      if(c>=97){
+        char insideLower= 'a'+(c-'a'+n);
+        char outsideLower= 'a'+(c-'a'+n)-26;
+        //inside the alphate
+        if(isalpha(insideLower)){
+            return insideLower;
+        }
+        //outside
+        else{
+          return outsideLower;
+        }
+      }
+      //rotate uppercase
+      else{
+        char insideUpper= 'A'+(c-'A'+n);
+        char outsideUpper= 'A'+(c-'A'+n)-26;
+        if(isalpha(insideUpper)){
+          return insideUpper;
+        }
+        else{
+          return outsideUpper;
+        }
+      }
+    }
+    //non-alphateic then return itself
+    else{
+       return c;
+    }
+}
 int main(int argc, char* argv[]) {
   if(argc != 3){
     fprintf(stderr, "wrong number of arguments\n");
     return 1;
   }
-  //char word[MAX_LEN];
+  char word[MAX_LEN];
+  memset(word,'\0',MAX_LEN);
 
   FILE* fin = fopen(argv[1],"r");
 
@@ -23,13 +55,26 @@ int main(int argc, char* argv[]) {
   // TODO: Write your code here to implement the encryption.
 int cin;
 int i = 0;
-int file[100];
+int count;
+int num;
+sscanf (argv[2],"%d",&num);
 while((cin = fgetc(fin))!= EOF){
-  file[i] = cin;
+  if(cin == ' ' || cin =='\n' ||'\0'){
+    for(count=strlen(word)-1;count>=0;count--){
+      printf("%c",rotate(word[count],num)); 
+    }
+    if(cin==' '){
+      printf(" ");
+    }
+    else if(cin=='\n'){
+      printf("\n");
+    }
+    memset(word,'\0',strlen(word));
+    i = 0;
+    continue;
+  }
+  word[i] = cin;
   i++;
-}
-for(int i = 0;i<100;i++){
-  printf("%d",file[i]);
 }
   // clean up gracefully
   fclose(fin);
