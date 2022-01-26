56e8408ab18d05ac21509625654980341add9ab0
skyeyestang99 <tommytang9958@gmail.com>
2022-01-25 21:25:44 -0800

    change1

--- encrypter.c
+++ encrypter.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_LEN 256 // maximum length of a single word
+//char rotate(char c, int n);
 
 int main(int argc, char* argv[]) {
-  char word[MAX_LEN];
+  if(argc != 3){
+    fprintf(stderr, "wrong number of arguments\n");
+    return 1;
+  }
+  //char word[MAX_LEN];
 
-  FILE* fin = fopen( /* TODO: Read from input file */ );
+  FILE* fin = fopen(argv[1],"r");
 
   if(fin == NULL) {
     perror("Could not open input file.");
@@ -15,7 +21,16 @@ int main(int argc, char* argv[]) {
   }
 
   // TODO: Write your code here to implement the encryption.
-
+int cin;
+int i = 0;
+int file[100];
+while((cin = fgetc(fin))!= EOF){
+  file[i] = cin;
+  i++;
+}
+for(int i = 0;i<100;i++){
+  printf("%d",file[i]);
+}
   // clean up gracefully
   fclose(fin);
 
